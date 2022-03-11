54fad643237edbc7790232165c9dd5cc7962740a
Amber-14 <amberyadav311@gmail.com>
2022-03-11 08:09:34 +0530

    Menu Items

---
+++ src/Reviews.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Component } from 'react'
+
+class Reviews extends Component {
+  render() {
+    return (
+      <div>Reviews</div>
+    )
+  }
+}
+
+export default Reviews
